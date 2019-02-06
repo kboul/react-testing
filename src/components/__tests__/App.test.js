a1f6554a81631a0d237fcc32a50c3d594851233c
kboul <kos.voulgaridis@gmail.com>
2019-02-06 14:34:43 +0200

    Write a test to ensure that CommentList instance exists inside App

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,10 +2,15 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import App from '../App'
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
+
+const wrapped = shallow(<App />)
 
 it('shows a comment box', () => {
-    const wrapped = shallow(<App />)
-
     // expect that inside App there is a CommentBox
     expect(wrapped.find(CommentBox).length).toEqual(1)
+})
+
+it('shows a comment list', () => {
+    expect(wrapped.find(CommentList).length).toEqual(1)
 })
