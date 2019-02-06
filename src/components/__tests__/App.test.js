d0477dbf7c31d02c22340db9e583e3b3fa892380
kboul <kos.voulgaridis@gmail.com>
2019-02-06 13:51:42 +0200

    Write a test to ensure that CommentBox instance is included inside App

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import App from '../App'
+import CommentBox from '../CommentBox';
 
 it('shows a comment box', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<App />, div)
+    const wrapped = shallow(<App />)
 
-    console.log(div.innerHTML)
-    expect(div.innerHTML).toContain('Comment Box')
-
-    ReactDOM.unmountComponentAtNode(div)
+    // expect that inside App there is a CommentBox
+    expect(wrapped.find(CommentBox).length).toEqual(1)
 })
