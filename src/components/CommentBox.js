2bb63d2a6e62cd56cc27680c3b0d39aed6b62644
kboul <kos.voulgaridis@gmail.com>
2019-02-13 15:36:15 +0200

    Refactor CommentBox to a class comp, create a textarea & a btn

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const CommentBox = () => {
-    return (
-        <div>Comment Box</div>
-    )
+class CommentBox extends Component {
+    state = {}
+    render() {
+        return (
+            <form>
+                <div className="form-group">
+                    <label for="addAComment">
+                        Add a comment
+                    </label>
+                    <textarea
+                        className="form-control"
+                        rows="3">
+                    </textarea>
+                </div>
+                <button
+                    type="button"
+                    className="btn btn-primary">
+                    Submit comment
+                </button>
+            </form>
+        )
+    }
 }
 
 export default CommentBox
