1e4b0e91db453de84e830093f8e318b431286599
kboul <kos.voulgaridis@gmail.com>
2019-02-13 14:31:38 +0200

    Use beforeEach on App.test.js to share some common code before test execution

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,7 +4,11 @@ import App from 'components/App'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
-const wrapped = shallow(<App />)
+let wrapped
+
+beforeEach(() => {
+    wrapped = shallow(<App />)
+})
 
 it('shows a comment box', () => {
     // expect that inside App there is a CommentBox
