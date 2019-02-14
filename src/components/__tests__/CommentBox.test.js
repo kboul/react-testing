755c988c8b784ced392f8165ff227ea1774d4209
kboul <kos.voulgaridis@gmail.com>
2019-02-14 17:11:32 +0200

    CommentBox - Write a test to check that users can type in the textarea

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -8,11 +8,21 @@ beforeEach(() => {
     wrapped = mount(<CommentBox />)
 })
 
+afterEach(() => {
+    wrapped.unmount()
+})
+
 it('has a text area and a buttton', () => {
     expect(wrapped.find('textarea').length).toEqual(1)
     expect(wrapped.find('button').length).toEqual(1)
 })
 
-afterEach(() => {
-    wrapped.unmount()
+it('has a text area and users can type in', () => {
+    wrapped.find('textarea').simulate('change', {
+        // pass as value the desired to be tested
+        target: { value: 'new comment' }
+    })
+    // force the component to update like using setState
+    wrapped.update()
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
 })
