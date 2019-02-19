add7d06235b395c814398e2d1cbc05d80b76e22d
kboul <kos.voulgaridis@gmail.com>
2019-02-19 17:26:12 +0200

    Wire up redux-promise middleware to store

--- src/Root.js
+++ src/Root.js
@@ -3,20 +3,21 @@ import { Provider } from 'react-redux'
 import { objectOf, arrayOf, string, element } from 'prop-types'
 
 import { createStore, applyMiddleware, compose } from 'redux'
+import reduxPromise from 'redux-promise'
 import reducers from 'redux-local/reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     || compose
 
 const Root = ({ children, initialState }) => {
-    const storeExpression = createStore(
+    const store = createStore(
         reducers,
         initialState,
-        composeEnhancers(applyMiddleware())
+        composeEnhancers(applyMiddleware(reduxPromise))
     )
 
     return (
-        <Provider store={storeExpression} >
+        <Provider store={store} >
             {children}
         </Provider>
     )
